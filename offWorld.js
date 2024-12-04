2ae108ba144e6f7c6e64c0d2695f101437cf61a9
RichardHayes <rhshadowgreen@gmail.com>
2024-12-03 19:08:39 -0800

    add ability to pass in server url

--- offWorld.js
+++ offWorld.js
@@ -1,23 +1,26 @@
-function rpcFactory(name) {
-    return async function rpc(...args) {
-        const res = await fetch("http://localhost:3000/cmd", {
-            method: "post",
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                cmd: name,
-                args
-            })
-        });
-        return res;
-    }
-}
-
 // Strings in the list below become keys on an exported object
 // that has the above rpc function as values
-export default [
-    'writeFile',
-    'second'
-].reduce((a, c) => { a[c] = rpcFactory(c); return a; }, {})
+export default function offWorld({ server }) {
+
+    function rpcFactory(name) {
+        return async function rpc(...args) {
+            const res = await fetch(`${server}/cmd`, {
+                method: "post",
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cmd: name,
+                    args
+                })
+            });
+            return res;
+        }
+    }
+
+    return [
+        'writeFile',
+        'second'
+    ].reduce((a, c) => { a[c] = rpcFactory(c); return a; }, {});
+}
